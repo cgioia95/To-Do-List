10be0efcadb894b5fde2c9a4430f7b36c51f9480
Christian Gioia <christiang101@gmail.com>
2019-02-14 19:25:44 +1100

    Added css, more pages and abstracted date functions to modules

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname +"/date.js");
+
 
 app = express();
 
-var items = ["Item 1", "Item 2", "Item 3"];
-
-app.use(bodyParser.urlencoded({extended: true}));
+const items = ["Item 1", "Item 2", "Item 3"];
+const workItems = [];
 
 
 app.set("view engine", "ejs");
 
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
 app.get("/", function(req, res) {
 
-  var today = new Date();
 
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
+  let day = date.getDay();
 
-  var day = today.toLocaleDateString("en-US", options);
-
-
-
-  res.render("list", {
-    kindOfDay: day,
-    newListItems: items
-  });
+  res.render("list", { listTitle : day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
-  item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+
+  let item = req.body.newItem;
+
+  console.log(req.body);
+
+  if (req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
 });
+
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work", newListItems: workItems});
+});
+
+app.get("/about", function(req,res){
+  res.render("about");
+});
+
 
 app.listen(3000, function(){
   console.log("Server live at Port 3000");
